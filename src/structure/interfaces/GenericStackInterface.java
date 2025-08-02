fd0b4b7df35456cb8388c3f1c9347f3c133b1d16
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-08-01 18:23:28 -0600

    The correct and completed interfaces of the Queue and Stack.

--- src/structure/interfaces/GenericStackInterface.java
+++ src/structure/interfaces/GenericStackInterface.java
@@ -1,51 +1,56 @@
 package src.structure.interfaces;
 
 /**
+ * @author Tenneh Kanneh
+ * Date: Fri Aug  1 18:22:37 MDT 2025
+ * 
  * A generic interface for a stack data structure (LIFO - Last In First Out).
  *
  * @param <T> the type of elements held in this stack
  */
 public interface GenericStackInterface<T> {
-
+    
     /**
-     * Pushes an item onto the top of the stack.
-     *
-     * @param item the item to be pushed
+     * Adds the specified element to the top of the stack.
+     * 
+     * @param element to be pushed onto the stack
      */
-    void push(T item);
+    void push(T element);
 
     /**
-     * Removes and returns the item at the top of the stack.
-     *
-     * @return the item removed from the top
-     * @throws java.util.NoSuchElementException if the stack is empty
+     * Removes and returns the top element from the stack.
+     * 
+     * @return the element remvoed from the stack.
      */
     T pop();
 
+    
     /**
-     * Returns the item at the top without removing it.
-     *
-     * @return the item at the top
-     * @throws java.util.NoSuchElementException if the stack is empty
+     * Returns without removing the top element of the stack.
+     * 
+     * @return the element of top of the stack
      */
     T peek();
 
     /**
-     * Checks whether the stack is empty.
-     *
-     * @return true if empty, false otherwise
+     * Returns true if the stack contains no elements.
+     * 
+     * @return true if the stack is empty
      */
     boolean isEmpty();
 
+
     /**
      * Returns the number of elements in the stack.
-     *
-     * @return the size of the stack
+     * @Return the number of elements in the stack
+     * @return
      */
     int size();
 
     /**
-     * Removes all elements from the stack.
+     * Returns a string representation of this stack.
+     * 
+     * @return a string representation of the stack
      */
-    void clear();
+    String toString();
 }
