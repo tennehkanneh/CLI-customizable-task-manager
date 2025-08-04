19a86a4f67e190e3303a6b7c44b42236e84df8e1
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-08-04 17:38:59 -0600

    Renaming the GenericLinkedList to GenericSingleLinkedList for better identification. Add unimplemented methods.

--- src/structure/GenericLinkedList.java
+++
@@ -1,7 +0,0 @@
-package src.structure;
-
-import src.structure.interfaces.GenericSingleLinkedListInterface;
-
-public class GenericLinkedList<T> implements GenericSingleLinkedListInterface<T> {
-    // Singly linked list
-}
---
+++ src/structure/GenericSingleLinkedList.java
@@ -0,0 +1,122 @@
+package src.structure;
+
+import src.structure.interfaces.GenericSingleLinkedListInterface;
+
+public class GenericSingleLinkedList<T> implements GenericSingleLinkedListInterface<T> {
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    @Override
+    public void add(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void add(int index, T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addAfter(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToFront(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addToRear(T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean contains(T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T first() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int indexOf(T element) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T last() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void set(int index, T element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    // Singly linked list
+    
+}
